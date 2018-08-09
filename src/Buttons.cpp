3018d042fab61db3a5521a1dae7eeb266b2d3a1a
Bartłomiej Żarnowski <thetosters@gmail.com>
2018-08-09 21:50:38 +0200

    Add button handling

---
+++ src/Buttons.cpp
@@ -0,0 +1,87 @@
+/*
+ MIT License
+
+ Copyright (c) 2018 Bartłomiej Żarnowski
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Buttons.cpp
+ Created on: Aug 9, 2018
+ Author: Bartłomiej Żarnowski (Toster)
+ */
+#include <Buttons.h>
+#include <Arduino.h>
+
+void Buttons::begin() {
+  if (pcf20) {
+    return;
+  }
+  pcf20 = new PCF8574(0x20);
+  pcf20->begin();
+  for(int t = 0; t < 8; t++) {
+    buttons[t].lastPressed = false;
+    buttons[t].longPressCallback = nullptr;
+    buttons[t].shortPressCallback = nullptr;
+    buttons[t].pressedTickCount = 0;
+  }
+}
+
+void Buttons::setButtonFunction(uint8_t index, Executable* shortPress, Executable* longPress) {
+  if (index < 8) {
+    buttons[index].longPressCallback = longPress;
+    buttons[index].shortPressCallback = shortPress;
+  }
+}
+
+void Buttons::update() {
+  if (millis() - lastMilis > BTN_READ_DELAY) {
+    lastMilis = millis();
+
+    uint8_t state = pcf20->read8();
+    for(int t = 0; t < 8; t++) {
+      //note inverted state! Button pressed is 0 not 1
+      handleButtonUpdate(buttons[t], bitRead(state, t) == 0);
+    }
+  }
+}
+
+void Buttons::handleButtonUpdate(ButtonContext& ctx, bool isPressed) {
+  if (ctx.lastPressed && isPressed) {
+    //user continue to hold pressed button
+    ctx.pressedTickCount ++;
+    ctx.pressedTickCount = ctx.pressedTickCount < 255 ? ctx.pressedTickCount : 254;
+
+  } else if ( (not ctx.lastPressed) and isPressed) {
+    //user just pressed button
+    ctx.lastPressed = true;
+    ctx.pressedTickCount = 1;
+
+  } else if ( ctx.lastPressed and (not isPressed)) {
+    //user just released button
+    Executable* toExec = (ctx.pressedTickCount > BTN_LONG_PRESS_TICKS) ?
+        ctx.longPressCallback : ctx.shortPressCallback;
+
+    if (toExec) {
+      toExec->execute();
+    }
+
+    ctx.lastPressed = false;
+    ctx.pressedTickCount = 0;
+  }
+}
---
+++ src/Buttons.h
@@ -0,0 +1,59 @@
+/*
+ MIT License
+
+ Copyright (c) 2018 Bartłomiej Żarnowski
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Buttons.h
+ Created on: Aug 9, 2018
+ Author: Bartłomiej Żarnowski (Toster)
+ */
+#ifndef Buttons_hpp
+#define Buttons_hpp
+
+#include <PCF8574.h>
+#include <Wire.h>
+#include "Executable.h"
+
+#define BTN_READ_DELAY (50)
+//time in ms after which long press is recognized
+#define BTN_LONG_PRESS_TICKS (1500 / BTN_READ_DELAY)
+
+class Buttons {
+  public:
+    void begin();
+    void update();
+    void setButtonFunction(uint8_t index, Executable* shortPress, Executable* longPress);
+  private:
+    struct ButtonContext {
+            Executable* shortPressCallback;
+            Executable* longPressCallback;
+            bool lastPressed;
+            uint8_t pressedTickCount;
+        };
+
+    PCF8574* pcf20 = nullptr;
+    long lastMilis = 0;
+    ButtonContext buttons[8];
+
+    void handleButtonUpdate(ButtonContext& ctx, bool isPressed);
+};
+
+#endif /* Buttons_hpp */
